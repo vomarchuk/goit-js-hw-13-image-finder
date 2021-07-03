9da72108702e2c14a09ba7c7cf3db69e16a759d7
Volodymyr Marchuk <vomarchuk@Volodymyrs-MacBook-Air.local>
2021-07-03 15:03:27 +0200

    решил сделать тему приложния is a not a Google

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './sass/main.scss';
 import './js/apiService';
+
+import 'material-icons/iconfont/material-icons.css';
+
 import imagesTpl from './tamplates/imagesTpl.hbs';
 import ImagesApiService from './js/apiService';
 
@@ -16,11 +19,21 @@ function onSearch(e) {
 
   imagesApiService.query = e.currentTarget.elements.query.value;
   imagesApiService.resetPage();
-  imagesApiService.fetchImages().then(images => {
-    appendImagesMarkup(images);
-  });
+  imagesApiService
+    .fetchImages()
+    .then(images => {
+      clearGalleryMarkup();
+      appendImagesMarkup(images);
+    })
+    .catch(error => {
+      console.log('errors:' + error);
+    });
 }
 
 function appendImagesMarkup(images) {
   refs.gallery.insertAdjacentHTML('beforeend', imagesTpl(images));
+}
+
+function clearGalleryMarkup() {
+  refs.gallery.innerHTML = '';
 }
